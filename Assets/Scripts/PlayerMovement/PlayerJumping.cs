4bbfd6e6eb1b0ddd2c8bc2ff53416feb2bcc1094
Ian Parker <ianmparker@outlook.com>
2017-02-18 13:59:00 -0500

    Moving Unity Project into Repository

---
+++ Assets/Scripts/PlayerMovement/PlayerJumping.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumping : MonoBehaviour {
+
+	public Vector2 jumpForce = new Vector2 (0, 300);
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space")){
+
+			GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+			GetComponent<Rigidbody2D> ().AddForce (jumpForce);
+		}
+	
+	}
+}
